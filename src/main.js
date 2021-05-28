da014ab4e463654150e0d99096f3cb48499c9759
rodener <rod.dajes@fourello.com>
2021-05-28 15:43:40 +0800

    Country Flag Shaders

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import * as THREE from 'three';
 import * as dat from 'dat.gui';
 import SceneManager from './sceneManager/scene';
 import gsap from 'gsap';
-
 const gui = new dat.GUI();
+
+//import shader
+import vertexShader from './shaders/test/vertex.glsl?raw';
+import fragmentShader from './shaders/test/fragment.glsl?raw';
 
 //scene
 const canvas = document.querySelector('#canvas');
@@ -13,51 +16,64 @@ scene.scene.background = 0x000000;
 scene.addOrbitControl();
 const axesHelper = new THREE.AxesHelper(5);
 
+
 //lights
 const directionalLight = new THREE.DirectionalLight(0xFFFFFF,1);
 directionalLight.position.set(10,10,10);
 scene.add(directionalLight);
-const directionalHelper = new THREE.DirectionalLightHelper( directionalLight, 5 );
-scene.add(directionalHelper);
+// const directionalHelper = new THREE.DirectionalLightHelper( directionalLight, 5 );
+//scene.add(directionalHelper);
 
-gui.add(directionalHelper, 'visible').name('DLight Helper')
+// gui.add(directionalHelper, 'visible').name('DLight Helper')
 
 const ambiantLight = new THREE.AmbientLight(0xFFFFFF,1);
 scene.add(ambiantLight);
 
-//geometry
+
+
+
+
 const width = 40;  
 const height = 40;   
-const geometry = new THREE.PlaneGeometry(width,height,100,100);
-const material = new THREE.MeshPhongMaterial( { color: 0x52ff45} );
+const geometry = new THREE.PlaneBufferGeometry(1,1,10,10);
+
+const count = geometry.attributes.position.count;
+const randoms = new Float32Array(count);
+
+for(let i=0; i < count; i++){
+	randoms[i] = Math.random();
+}
+
+geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms,1));
+
+
+const material = new THREE.RawShaderMaterial({
+	vertexShader:vertexShader,
+	fragmentShader:fragmentShader,
+	uniforms:{
+		uFrequency:{ value: new THREE.Vector2(10, 5) },
+		uTime:{ value: 0},
+		uColor:{ value: new THREE.Color('orange')},
+		uTexture:{ value: new THREE.TextureLoader().load('/texture/Philippines-Flag.png')}
+	},
+	side:THREE.DoubleSide
+});
 const plane = new THREE.Mesh(geometry,material);
-plane.rotation.x = Math.PI * 1.50;
+// plane.rotation.x = Math.PI * 1.50;
 scene.add(plane);
 
-
-gui.add(material, 'wireframe').name('Plane WireFrame');
-
-
-const boxGeometry = new THREE.BoxBufferGeometry(4,4,4);
-const boxMaterial = new THREE.MeshStandardMaterial({color:0x442255});
-const cube = new THREE.Mesh(boxGeometry, boxMaterial);
-cube.position.y = 5;
-cube.add(axesHelper);
-scene.add(cube);
-
-gui.add(boxMaterial, 'wireframe').name('Box Wireframe');
-gui.add(cube.scale, 'x').min(1).max(5).step(0.01).name('BoxScale X');
-gui.add(cube.scale, 'y').min(1).max(5).step(0.01).name('BoxScale Y');
-gui.add(cube.scale, 'z').min(1).max(5).step(0.01).name('BoxScale Z');
-
-gsap.to(cube.rotation,{ duration:3,delay:3,x: Math.PI * 2 });
+gui.add(material,'wireframe');
+gui.add(material.uniforms.uFrequency.value, 'x').min(1).max(20).name('frequencyX');
+gui.add(material.uniforms.uFrequency.value, 'y').min(1).max(20).name('frequencyY');
+// gui.addColor(material.uniforms.uColor.value,'color').name('Color');
 
 const clock = new THREE.Clock();
 
 const animate = () => {
 	const elapsedTime = clock.getElapsedTime();
 
-	cube.rotation.y = elapsedTime;
+	//update materials
+	material.uniforms.uTime.value = elapsedTime;
 
 	
 	scene.onUpdate();
--- src/sceneManager/scene.js
+++ src/sceneManager/scene.js
@@ -5,8 +5,8 @@ import Stats from 'stats.js';
 export default class SceneManager {
 
     constructor(canvas){
-         this.canvas = canvas;
-         this._init();
+        this.canvas = canvas;
+        this._init();
     }
 
     _init(){
@@ -20,9 +20,9 @@ export default class SceneManager {
         window.addEventListener('resize', () => this.onWindowsResize(), false);
 
         const camera = new THREE.PerspectiveCamera(75 ,window.innerWidth / window.innerHeight,0.1,1000);
-        camera.position.z = 20;
-        camera.position.y = 5;
-        camera.lookAt(0,0,0);
+        // camera.position.y = 5;
+        camera.position.z = 5;
+        // camera.lookAt(0,0,0);
 
         //init stats
         let stats = new Stats();
